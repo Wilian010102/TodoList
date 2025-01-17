244c84ae50eadf8eac2c48a5bbe7b6bf49f067f0
Will <102735027+Wilian010102@users.noreply.github.com>
2025-01-17 17:09:00 +0700

    delete

--- middleware/auth.go
+++ middleware/auth.go
@@ -9,7 +9,7 @@ import (
 
 func Auth() gin.HandlerFunc {
     return func(c *gin.Context) {
-        // Ambil header Authorization
+       
         tokenString := c.GetHeader("Authorization")
         if tokenString == "" {
             c.JSON(http.StatusUnauthorized, gin.H{"error": "Header Authorization tidak ditemukan"})
@@ -17,17 +17,17 @@ func Auth() gin.HandlerFunc {
             return
         }
 
-        // Validasi format token (harus diawali dengan "Bearer ")
+        
         if !strings.HasPrefix(tokenString, "Bearer ") {
             c.JSON(http.StatusUnauthorized, gin.H{"error": "Format token tidak valid. Harus diawali dengan 'Bearer '"})
             c.Abort()
             return
         }
 
-        // Hilangkan prefix "Bearer " untuk mendapatkan token sebenarnya
+       
         tokenString = strings.TrimPrefix(tokenString, "Bearer ")
 
-        // Verifikasi token menggunakan utils
+
         claims, err := utils.VerifyToken(tokenString)
         if err != nil {
             c.JSON(http.StatusUnauthorized, gin.H{"error": "Token tidak valid atau sudah kadaluarsa"})
@@ -35,10 +35,10 @@ func Auth() gin.HandlerFunc {
             return
         }
 
-        // Simpan informasi pengguna ke dalam context (opsional)
+
         c.Set("user_id", claims.UserID)
 
-        // Lanjutkan ke handler berikutnya
+
         c.Next()
     }
 }
--- models/user.go
+++ models/user.go
@@ -2,8 +2,7 @@ package models
 
 import "gorm.io/gorm"
 
-// User represents a user model
-// @Description User model
+
 type User struct {
     gorm.Model
     Username string `json:"username" gorm:"unique"`
